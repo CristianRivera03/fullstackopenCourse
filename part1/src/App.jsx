42afdb8a98584daca81dbddb4e2b92b66ea50736
CristianRivera03 <cr8539788@gmail.com>
2025-10-25 19:07:48 -0600

    1.4: Información del Curso paso 4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,17 +8,7 @@ const Header = (props) => {
   );
 };
 
-export const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercise={props.exercise1} />
-      <Part part={props.part2} exercise={props.exercise2} />
-      <Part part={props.part3} exercise={props.exercise3} />
-    </>
-  );
-};
-
-export const Part = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
@@ -28,45 +18,44 @@ export const Part = (props) => {
   );
 };
 
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
-    </p>
+const Content = (props) => {
+  const p = props.content 
+  return(
+    p.map((p) => <Part part={p.name.toString()} exercise={p.exercises.toString()} />)
   );
+};
+
+
+const Total = (props) => {
+  let total = 0;
+  const p = props.sum;
+  p.map((p) => (total += p.exercises));
+  return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
   // Variables at the top
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
   return (
     <>
       <Header nameCourse={course} />
-      <Content
-        part1={part1.name}
-        exercise1={part1.exercises}
-        part2={part2.name}
-        exercise2={part2.exercises}
-        part3={part3.name}
-        exercise3={part3.exercises}
-      />
-      <Total
-        exercise1={part1.exercises}
-        exercise2={part2.exercises}
-        exercise3={part3.exercises}
-      ></Total>
+      <Content content={parts} />
+      <Total sum={parts} />
     </>
   );
 };
