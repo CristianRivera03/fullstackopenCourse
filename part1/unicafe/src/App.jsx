7433df240e028ce465f582dd5d1f94833e97095d
CristianRivera03 <cr8539788@gmail.com>
2025-11-09 14:15:36 -0600

    1.7: unicafe, paso 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,56 @@
 import { useState } from "react";
 
+const Average = ({data , cantidad}) => {
+  if(data == 0){
+    return <p>No hay informacion disponible para mostrar</p>
+  }else{
+    return <p>average {data / cantidad}</p>
+  }
+}
+
+const Positive = ({good , cantidad}) =>{
+
+  if(good == 0){
+    return <p>No hay informacion disponible para mostrar</p>
+  }else{
+    return <p>positive {(good * 100) / cantidad}</p>
+  }
+
+}
+
+
+
+
+
 const App = () => {
-  const [clicks, setClicks] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    allClicks: 0,
+    average: 0,
+  });
   const handlerButtonGood = () =>
-    setClicks({ ...clicks, good: clicks.good + 1 });
+    setClicks({
+      ...clicks,
+      good: clicks.good + 1,
+      allClicks: clicks.allClicks + 1,
+      average: (clicks.average + 1),
+    });
   const handlerButtonNeutral = () =>
-    setClicks({ ...clicks, neutral: clicks.neutral + 1 });
-  const handlerButtonBad = () => setClicks({ ...clicks, bad: clicks.bad + 1 });
+    setClicks({
+      ...clicks,
+      neutral: clicks.neutral + 1,
+      allClicks: clicks.allClicks + 1,
+      average: (clicks.average + 0) ,
+    });
+  const handlerButtonBad = () =>
+    setClicks({
+      ...clicks,
+      bad: clicks.bad + 1,
+      allClicks: clicks.allClicks + 1,
+      average: (clicks.average - 1),
+    });
 
   return (
     <div>
@@ -20,6 +64,10 @@ const App = () => {
       <p>good {clicks.good}</p>
       <p>neutral {clicks.neutral}</p>
       <p>bad {clicks.bad}</p>
+
+      <p>all {clicks.allClicks}</p>
+      <Average data = {clicks.average} cantidad = {clicks.allClicks}/>
+      <Positive good = {clicks.good} cantidad={clicks.allClicks}/>
     </div>
   );
 };
